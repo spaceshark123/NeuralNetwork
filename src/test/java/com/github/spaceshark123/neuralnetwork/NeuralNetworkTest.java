1f611401a0376932e2ed156b5cb5c1a59ed2fc7b
spaceshark123 <amogh.patnaik@icloud.com>
2025-12-15 01:54:09 -0600

    add maven wrapper, remove physical jar dependencies, use shading to create a fat-jar executable

--- src/test/java/com/github/spaceshark123/neuralnetwork/NeuralNetworkTest.java
+++ src/test/java/com/github/spaceshark123/neuralnetwork/NeuralNetworkTest.java
@@ -2,6 +2,8 @@ package com.github.spaceshark123.neuralnetwork;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
 
 public class NeuralNetworkTest {
 
@@ -9,5 +11,11 @@ public class NeuralNetworkTest {
 	public void testAddition() {
 		int sum = 2 + 3;
 		Assertions.assertEquals(5, sum);
+	}
+
+	@Test
+	public void testJFreeChartAvailability() {
+		JFreeChart chart = ChartFactory.createPieChart("Test Chart", null, true, true, false);
+		Assertions.assertNotNull(chart);
 	}
 }
